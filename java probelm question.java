99d3edc18f4c8bc25a5b2978932e9e26d9144913
kaushal prajapti <kaushalprajapati.in@outlook.com>
2025-08-13 15:21:50 +0530

    msg

---
+++ java probelm question.java
@@ -0,0 +1,94 @@
+//public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Welcome to Bridgelabz");
+//     }
+//}
+
+// public class Main{
+//     public static void main(String[] args) {
+//      Scanner input = new Scanner(System.in)
+//      int num1 = input.nextInt();
+//      int num2 = input.nextInt(); 
+//      int sum = num1 + num2;
+//      System.out.println("Sum is: " + sum);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int Celsius = input.nextInt();
+//         int Fahrenheit = (Celsius * 9/5) + 32;
+//         System.out.println(Fahrenheit);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int radius = input.nextInt();
+//         double area = Math.PI * radius * radius;
+//         System.out.printf("Area of circle: %.2f%n", area);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int radius = input.nextInt();
+//         int height = input.nextInt();
+//         int volume = (int)(Math.PI * radius * radius * height);
+//         System.out.printf("Volume of cylinder: %d%n", volume);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         principal = input.nextInt();
+//         rate = input.nextInt();
+//         time = input.nextInt();
+//         double simpleInterest = (principal * rate * time) / 100.0;
+//         System.out.printf("Simple Interest: %.2f%n", simpleInterest);
+//     }
+// }
+
+// public class Main {
+//      public static void main(String[] args) {
+//          Scanner input = new Scanner(System.in);
+//          int length = input.nextInt(
+//         int width = input.nextInt();
+//         int perimeter = 2 * (length + width);
+//          System.out.printf("Perimeter of rectangle: %d%n", perimeter);
+//      }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int base = input.nextInt();
+//         int exponent = input.nextInt();
+//         int result = 1;
+//         result = (int) Math.pow(base, exponent);
+//         System.out.printf("Result of %d raised to the power of %d is: %d%n", base, exponent, result);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int num1 = input.nextInt();
+//         int num2 = input.nextInt();
+//         int num3 = input.nextInt();
+//         int avg = (num1 + num2 + num3) / 3;
+//         System.out.printf("Average of %d, %d, and %d is: %d%n", num1, num2, num3, avg)
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int kilometers = input.nextInt();
+//         double miles = kilometers * 0.621371;   
+//         System.out.printf("Distance in miles: %.2f%n", miles);
+//     }
+// }
---
+++ practice question.py
@@ -0,0 +1,25 @@
+#print("Welcome to Bridgelabz")
+
+#num1 = int(input("enter first number:"))
+##num2 = int(input("enter second number:"))
+#sum = num1 + num2
+#print("sum:", sum)
+
+#celsius = float(input("enter the temperature in celsius:"))
+#fahrenheit = (celsius * 9/5) + 32
+#print("Temperature in Fahrenheit is:", fahrenheit)
+
+#radius = float(input("Enter the radius of the circle: "))
+#area = 3.14 * radius * radius
+#print("Area of the circle is:", area)
+
+#radius = float(input("Enter the radius of the circle: "))
+#height = float(input("Enter the height of the cylinder: "))
+#volume = 3.14 * radius * radius * height
+#print("volume of the cylinder is:", volume)
+
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest: "))
+time = float(input("Enter the time in years: "))
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest is:", simple_interest)
